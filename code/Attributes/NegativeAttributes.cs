9508b01952227da8579cc777d0efbf874b20d666
TheDarkElites <thedarkredstone@gmail.com>
2025-12-11 04:40:10 -0500

    negative attributes

--- code/Attributes/NegativeAttributes.cs
+++ code/Attributes/NegativeAttributes.cs
@@ -17,7 +17,7 @@ public class IncreasedMobSpeedAttribute : Attribute
     public IncreasedMobSpeedAttribute(SceneTree sceneTree) : base(sceneTree, "Increased Mob Speed", "Enemies now move 25% faster.") {}
     public override bool Activate()
     {
-        Globals.Instance.MobSpeed *= (float)1.25;
+        Globals.Instance.MobSpeed *= 1.25f;
         SendUpdate();
         return true;
     }
@@ -30,6 +30,39 @@ public class IncreasedMobDamageAttribute : Attribute
     {
         Globals.Instance.MobDamageRate *= 1.25;
         SendUpdate();
+        return true;
+    }
+}
+
+public class IncreasedBulletSpreadAttribute : Attribute
+{
+    public IncreasedBulletSpreadAttribute(SceneTree sceneTree) : base(sceneTree, "More Bullet Spread", "Increases bullet spread by 15%.") {}
+    public override bool Activate()
+    {
+        Globals.Instance.BulletSpread *= 1.15f;
+        SendUpdate();
+        return true;
+    }
+}
+
+public class IncreaseSpawnRateAttribute : Attribute
+{
+    public IncreaseSpawnRateAttribute(SceneTree sceneTree) : base(sceneTree, "More Zombies", "Increases zombie spawnrate by 10%.") {}
+    public override bool Activate()
+    {
+        Timer spawnTimer = SceneTree.GetRoot().GetNode<Timer>("/root/Game/Timer");
+        spawnTimer.WaitTime *= 0.90f;
+        return true;
+    }
+}
+
+public class IncreaseMobRangeAttribute : Attribute
+{
+    public IncreaseMobRangeAttribute(SceneTree sceneTree) : base(sceneTree, "Increase Mob Range", "Mobs can now damage the player from farther away.") {}
+    public override bool Activate()
+    {
+        Area2D hurtArea = SceneTree.GetRoot().GetNode<Area2D>("/root/Game/Player/HurtBox");
+        hurtArea.Scale *= 1.15f;
         return true;
     }
 }
