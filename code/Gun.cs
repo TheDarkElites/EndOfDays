e8c9c1cf1f6aa11fa5561b1c8f0311d15d834f5a
TheDarkElites <thedarkredstone@gmail.com>
2025-12-10 22:09:45 -0500

    added texture changing for the gun

--- code/Gun.cs
+++ code/Gun.cs
@@ -9,16 +9,31 @@ public partial class Gun : Area2D
 	public Marker2D ShootingPoint;
 
 	[Export] private Timer _timer;
+	[Export] 
+	private AnimatedSprite2D _animation;
+	
+	private Dictionary<Vector2, StringName> _animationDictionary = new Dictionary<Vector2,StringName>();
 
 	private bool coolingDown = false;
 	public override void _Ready()
 	{
 		_bulletScene = ResourceLoader.Load<PackedScene>("res://scenes/bullet.tscn");
 		_timer.Timeout += () => CooldownDone();
+		
+		_animationDictionary[Vector2.Down] = "down";
+		_animationDictionary[Vector2.Up] = "up";
+		_animationDictionary[Vector2.Left] = "left";
+		_animationDictionary[Vector2.Right] = "right";
+		_animationDictionary[Vector2.Zero] = "down";
 	}
 	public override void _PhysicsProcess(double delta)
 	{
 		LookAt(GetGlobalMousePosition());
+		
+		Vector2 mouseDirection = Vector2.Right
+			.Rotated(Mathf.Round(_animation.GetGlobalRotation() / (float)Math.Tau * 4) *
+				(float)Math.Tau / 4).Snapped(Vector2.One);
+		_animation.Play(_animationDictionary[mouseDirection]);
 		base._PhysicsProcess(delta);
 	}
 
